f6201f5c29338d3b6a588c1972a80aa69e186250
H. Turcan Cicek <htcicek@users.noreply.github.com>
2020-06-03 15:33:16 -0400

    wrapped word list in agent

--- dictionary/lib/dictionary/word_list.ex
+++ dictionary/lib/dictionary/word_list.ex
@@ -1,9 +1,13 @@
 defmodule Dictionary.WordList do
 
-  def random_word(words) do
-    words
-    |> Enum.random()
+  def start_link() do
+    Agent.start_link(&word_list/0)
   end
+
+  def random_word(agent) do
+    Agent.get(agent, &Enum.random/1)
+  end
+
   def word_list do
     "../../assets/words.txt"
     |> Path.expand(__DIR__)
